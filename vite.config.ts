2d80f278f11eb2487034bd8f7503ffae6d4e7c25
June <1601745371@qq.com>
2023-02-16 21:33:52 +0800

    chore: 打包配置优化

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@
  * @Description:
  * @Date: 2023-01-17 12:46:35
  * @LastEditors: June
- * @LastEditTime: 2023-02-16 14:15:39
+ * @LastEditTime: 2023-02-16 21:32:51
  */
-import { defineConfig } from 'vite';
+import type { ConfigEnv, UserConfigExport } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import * as path from 'path';
 import AutoImport from 'unplugin-auto-import/vite';
@@ -14,51 +14,52 @@ import viteCompression from 'vite-plugin-compression';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [
-        vue(),
-        AutoImport({
-            resolvers: [ElementPlusResolver()],
-        }),
-        Components({
-            resolvers: [ElementPlusResolver()],
-        }),
-        viteCompression(),
-    ],
-    resolve: {
-        //设置别名
-        alias: {
-            '@': path.resolve(__dirname, 'src'),
+export default ({ command }: ConfigEnv): UserConfigExport => {
+    return {
+        plugins: [
+            vue(),
+            AutoImport({
+                resolvers: [ElementPlusResolver()],
+            }),
+            Components({
+                resolvers: [ElementPlusResolver()],
+            }),
+            viteCompression(),
+        ],
+        resolve: {
+            //设置别名
+            alias: {
+                '@': path.resolve(__dirname, 'src'),
+            },
+            // 忽略后缀名的配置
+            // extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
         },
-        // 忽略后缀名的配置
-        // extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
-    },
-    css: {
-        preprocessorOptions: {
-            scss: {
-                additionalData: '@import "@/styles/variable.scss";',
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: '@import "@/styles/variable.scss";',
+                },
             },
         },
-    },
-    server: {
-        // 设置 https 代理
-        proxy: {
-            '/api': {
-                target: 'http://110.41.150.71:3000',
-                changeOrigin: true,
-                rewrite: (path: string) => path.replace(/^\/api/, ''),
+        server: {
+            // 设置 https 代理
+            proxy: {
+                '/api': {
+                    target: 'http://110.41.150.71:3000',
+                    changeOrigin: true,
+                    rewrite: (path: string) => path.replace(/^\/api/, ''),
+                },
             },
         },
-    },
-
-    build: {
-        minify: 'terser',
-        terserOptions: {
-            compress: {
-                //生产环境时移除console
-                drop_console: true,
-                drop_debugger: true,
+        build: {
+            minify: 'terser',
+            terserOptions: {
+                compress: {
+                    //生产环境时移除console
+                    drop_console: true,
+                    drop_debugger: true,
+                },
             },
         },
-    },
-});
+    };
+};
